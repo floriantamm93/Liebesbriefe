465157de4a0074cd4d40010892a2d45335d7987c
Florian Tamm <floriantamm93@users.noreply.github.com>
2026-09-06 00:34:21 +0200

    05 September

--- js/script.js
+++ js/script.js
@@ -1157,6 +1157,59 @@ https://www.instagram.com/reels/Db6sB0oTwtO/
     `),
         closing: "Bis Bald!",
         signature: "Der Dussel Flo 🖤"
+    },
+    {
+        id: "2026-09-05",
+        date: "05. September 2026",
+        time: "23:30",
+        title: "Für meine besondere Person",
+        salutation: "Heut ein Gedicht für dich",
+        paragraphs: toParagraphs(`
+
+        Was geeeeht ich hoffe du hast nen schönen Tag gehabt.
+        Heute habe ich mal wieder ein Gedicht für dich vorbereitet
+        was ein bisschen dich beschreibt wie ich dich wahr nehme.
+
+        Es gibt Schönheit, die im Tageslicht vergeht,
+        und Schönheit die erst in der Dunkelheit entsteht.
+        Doch deine scheint weder Sonne noch Mond zu brauchen,
+        als würde sie aus etwas Eigenem auftauchen.
+
+        Dein Lächeln ist so sanft wie schwarze Seide
+        auf der Haut nachdem man frisch geduscht sich kleidet.
+        Chantal das ist etwas worum ich dich beneide
+        weil sich mein Herz stets für dich entscheidet.
+
+        Dein Herz was viel mehr fühlt als es sollte,
+        deine Seele die viel zu viuel mit sich trägt
+        sind genau die Mischung die ich immer wollte
+        weil sie etwas in meinem inneren bewegt.
+
+        Deine Energie dein Stil deine Art
+        ziehen mich immer wieder in deinen Bann
+        so eigen, so wild und manchmal zart
+        auf eine Weise die ich nicht erklären kann.
+
+        Chantal ich weiß du hast es manchmal hart
+        und du verdienst jemanden der all das sieht
+        einen der dich hält wenn das Chaos sich offenbart
+        und dich dann noch liebt selbst wenn ein schlechter Tag geschieht
+
+        Nun finde erst den Platz der wirklich zu dir passt
+        ganz ohne Ketten die dich irgendwo festhalten
+        verbieg dich nicht für Menschen nur weil du sie hast
+        und fang an dein eigenes Leben richtig zu gestalten.
+
+        
+        Das war es für heute ich hoffe es gefällt dir 🫶🏼
+        Ich sitz hier und schwitze ey es is crazy wie 
+        viel Hirnschmalz das kostet :D
+        Ach und auf diesen Wege erinnere ich dich an die drei Fragen
+        die du mir noch stellen kannst 🙂
+
+    `),
+        closing: "Warum liegt hier Stroh",
+        signature: "Das war wohl Flo 🖤"
     }
 ];
 
