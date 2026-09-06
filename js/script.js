b8eaf82fc32f81a95dec80679678f07bb3480292
Florian Tamm <floriantamm93@users.noreply.github.com>
2026-09-06 21:42:31 +0200

    06 September

--- js/script.js
+++ js/script.js
@@ -1210,6 +1210,48 @@ https://www.instagram.com/reels/Db6sB0oTwtO/
     `),
         closing: "Warum liegt hier Stroh",
         signature: "Das war wohl Flo 🖤"
+    },
+    {
+        id: "2026-09-06",
+        date: "06. September 2026",
+        time: "19:30",
+        title: "English ok?",
+        salutation: "A pleasure knowing you",
+        paragraphs: toParagraphs(`
+
+        Today I wanna try something different.
+        Did you know that people have different
+        characters when they switch languages?
+        Thats why I wanted to try writing you a letter in english.
+
+        Now that I switched the language I got alot of
+        new words to describe how special you are to me 🫶🏼
+        You are funny, kindhearted, compassionate, inspiring,
+        gorgeous and have alot of special traits which I all love.
+
+        You are attractive as fuck and hotter than the sun,
+        with or without cosmetics. 
+        Another thing about you is your view of the world
+        and how it seems like you know so much stuff
+        about movies, series and in general so much stuff
+        like woodcrafting and creatifity.
+
+        Oh, and the way you decorate your rooms and
+        your ideas for so many different things like wall paint,
+        bedframe and querki pieces for every place on the cupboard.
+
+        It is so frustrating to be not in your area since I
+        would like to spent more days with you by my side.
+        Luckily for me the loyg is right around the corner 😊
+        I hope you look forward to enjoy that day as much as I do!
+
+        Your day today started late because of alot of internal conflicts
+        so I wish you alot of good energy to feel good asap!
+
+
+    `),
+        closing: "See you soon!",
+        signature: "The Mephistopheles Flo 🖤"
     }
 ];
 
