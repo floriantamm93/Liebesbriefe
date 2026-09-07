2c4fde5cd2cf69b2ec3cbd260b36cf40a675f636
Florian Tamm <floriantamm93@users.noreply.github.com>
2026-09-07 23:32:25 +0200

    07 September

--- js/script.js
+++ js/script.js
@@ -1252,6 +1252,58 @@ https://www.instagram.com/reels/Db6sB0oTwtO/
     `),
         closing: "See you soon!",
         signature: "The Mephistopheles Flo 🖤"
+    },
+
+    {
+        id: "2026-09-07",
+        date: "07. September 2026",
+        time: "17:30",
+        title: "Back to the roots",
+        salutation: "Na du bezaubernd schöne!",
+        paragraphs: toParagraphs(`
+       
+        Ich hoffe deine Unterleibschmerzen sind
+        nicht mehr so aktiv wenn du das hier liest.
+        Gestern hast du mir echt Sorge bereitet als 
+        du auf einmal nicht mehr geantwortet hast :/
+        Mir gingen soviele Szenarien durch den Kopf
+        was vorgefallen sein könnte. Aber zum Glück
+        bist du "Nur" weggepennt 😂
+
+        Heute möchte ich dir einfach mal danke
+        sagen das du in meinem Leben aufgetaucht bist.
+        Du magst es vielleicht nicht so wahrnehmen 
+        aber ich bin wirklich froh dich getroffen zu haben.
+
+        Ich werde nur leider das unangenehme Gefühl nicht los,
+        dass du die Sachen die ich sage nicht ernst nimmst.
+        Wenn ich zum Beispiel sage, du siehst ohne Make-up
+        auch schon hot as fuck aus dann meine ich das wirklich ne.
+        Das du diesbezüglich gerade bei neuen Leuten und besonders
+        bei Männern misstrauisch bist verstehe ich voll und ganz.
+        Das gilt nicht nur für dein Gesicht sondern generell für 
+        dein gesamt Erscheinen. In meinen AUgen bist du einfach 
+        wunderschön, sexy und einzigartig ♥️
+
+    `),
+        closing: "Mach dir ne schöne Nacht!",
+        signature: "Dein nicht so heimlicher verehrer Flo 😝"
+    },
+    {
+        id: "2026-09-07.2",
+        date: "07.2. September 2026",
+        time: "17:30",
+        title: "Back to the roots",
+        password: "Sexyschmexy",
+        passwordHint: "About you",
+        salutation: "Na du bezaubernd schöne!",
+        paragraphs: toParagraphs(`
+       
+        Verarscht.
+
+    `),
+        closing: "See you soon!",
+        signature: "The Mephistopheles Flo 🖤"
     }
 ];
 
@@ -1362,6 +1414,13 @@ function selectLetter(id) {
 }
 
 function openLetter() {
+    if (selectedLetter.password) {
+        const eingabe = prompt("Dieser Brief ist versiegelt. Passwort eingeben:");
+        if (eingabe !== selectedLetter.password) {
+            alert("Das Passwort ist leider falsch.");
+            return;
+        }
+    }
   if (openButton.disabled) return;
   renderLetter(selectedLetter);
   openButton.disabled = true;
