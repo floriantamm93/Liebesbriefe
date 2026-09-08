a3ef5d5e076150e0fd9e93f0a0477c19ab3286b6
Florian Tamm <floriantamm93@users.noreply.github.com>
2026-09-08 23:15:05 +0200

    08 September

--- js/script.js
+++ js/script.js
@@ -1253,7 +1253,6 @@ https://www.instagram.com/reels/Db6sB0oTwtO/
         closing: "See you soon!",
         signature: "The Mephistopheles Flo 🖤"
     },
-
     {
         id: "2026-09-07",
         date: "07. September 2026",
@@ -1288,6 +1287,61 @@ https://www.instagram.com/reels/Db6sB0oTwtO/
     `),
         closing: "Mach dir ne schöne Nacht!",
         signature: "Dein nicht so heimlicher verehrer Flo 😝"
+    },
+    {
+        id: "2026-09-08",
+        date: "08. September 2026",
+        time: "22:56",
+        title: "Bald is Loyg bald is Loyg!",
+        salutation: "Ich grüße!",
+        paragraphs: toParagraphs(`
+       
+        Heute habe ich einige Reels für dich,
+        die mich an dich denken lasse und die 
+        als gut empfinde bzw. passend fand.
+        Guck die  dir nur an wenn du stable
+        bist. Bitte immer von Slide 1 angucken 🙏🏼
+
+        Das erste ist schon ziemlich treffend
+        bzw so sehe ich wie du dich fühlst 🫶🏼
+
+        https://www.instagram.com/p/Dcxfb-loEMQ/?igsi=bjdkNnJuY2tyZWg0
+
+        Und darauf kommt direkt das hier:
+
+        https://www.instagram.com/p/DcOt9MAj1CT/?stkn=MWp3eGhwbmh2dGIweQ%3D%3D&img_index=1
+
+        Dann schlagen wir die connection zu mir
+
+        https://www.instagram.com/p/Dc1gU2ilpYg/?img_index=1&stkn=MTE4NWZ2cXJvendubQ%3D%3D
+
+        💜🖤
+
+        Wenn ich einen Wunsch hätte dann das du das hier
+        aktzeptieren kannst:
+
+        https://www.instagram.com/p/Dc0N29PGpgr/?img_index=1&igsi=MWFuc3ZxdWJ3ZThqMg%3D%3D
+
+        Denn:
+
+        https://www.instagram.com/p/Dcq7iW2Ghui/?img_index=2&stkn=eDNkejRnY280aHp3
+
+        Weil:
+
+        https://www.instagram.com/p/Dc2qAMmNGNz/?igsi=MTRpdW8yeWJ5eTd6OQ%3D%3D
+
+        Ich denke nämlich wirklich hier dran und möchte dir das geben
+
+        https://www.instagram.com/p/DcY5-sts2ec/?stkn=MTl6cHJwb3czZ3B2eA%3D%3D
+
+        Ansonsten passiert das hier 😈
+
+        https://www.instagram.com/reels/Dcyr6pPxRbj/
+
+
+    `),
+        closing: "Bis demnächst 😁",
+        signature: "Dein Flo 🖤💜"
     }
 ];
 
