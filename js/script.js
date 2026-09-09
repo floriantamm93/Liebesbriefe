b6cdb3195bd21a727f0ee13e2fda05f71439eb58
Florian Tamm <floriantamm93@users.noreply.github.com>
2026-09-10 00:26:56 +0200

    09 September

--- js/script.js
+++ js/script.js
@@ -1342,6 +1342,50 @@ https://www.instagram.com/reels/Db6sB0oTwtO/
     `),
         closing: "Bis demnächst 😁",
         signature: "Dein Flo 🖤💜"
+    },
+    {
+        id: "2026-09-09",
+        date: "09. September 2026",
+        time: "23:56",
+        title: "Muskelkater",
+        salutation: "Du schon wieder. Nein doch?!",
+        paragraphs: toParagraphs(`
+       
+        Boah heute ist es wirklich schwierig Worte zu finden.
+        Die Sache ist ich weiß nicht ob es komisch
+        rüberkommt wenn ich teilweise immer wieder
+        die selben Sachen schreibe. Ich könnte dir jeden Tag
+        sagen wie unglaublich ich dich finde und ich denke auch,
+        dass du das mittlerweile verstanden hast wie wichtig du mir bist.
+
+        Auch glaube ich wenn ich das immer wieder wiederhole
+        verliert es an Bedeutung und das möchte ich irgendwie nicht.
+        Und dann kommt wiederum - zumindest glaub ich, dass es bei dir der
+        Fall ist was normal wäre - das Gefühl, dass meine Gefühle für dich 
+        abschwächen was definitiv nicht der Fall ist.
+        Sogar eher das Gegenteil. Jeden Tag den ich mit dir verbringe,
+        ob es nur per WhatsApp schreiben oder Anime gucken oder
+        sonst was ist merke ich, wie unglaublich du bist.
+        Als Person als Geist als Seele hast du mich einfach
+        geschnappt ohne es vielleicht selbst zu bemerken ☺️
+        Kein Plan um ehrlich zu sein wie awkward das vielleicht
+        für dich ist aber so fühle ich für dich.
+
+        Und mein Gott bist du schön. Aphrodite steht ja mal sowas
+        von in deinem Schatten. Mag sein dass du nicht die gesellschaftlichen
+        Schönheitsideale hast aber wenn ich Bilder von dir sehe
+        oder dich private treffe kommt es mir so vor als würde ich
+        in ein Welt blicken in der es nur so vor Einzigartigkeit, Stil,
+        Klasse und Faszination. So und das du halt wirklich hot as fuck bist
+        sollte ja wohl selbst ein Blinder erkennen.
+
+        Heute mal bisl anderer Vibe hoffe das ist fine für dich 🤗🫶🏼
+
+        https://open.spotify.com/intl-de/track/2gT72HL4Y84K2Mle3miAMJ?si=6e4ab160636d40e7
+
+    `),
+        closing: "Du bist ein Traum",
+        signature: "Dieser Flo 😝"
     }
 ];
 
