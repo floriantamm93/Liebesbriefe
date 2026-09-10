4d8778490ad5c264bec70a62ecb092065b737b1d
Florian Tamm <floriantamm93@users.noreply.github.com>
2026-09-10 23:35:43 +0200

    10 September

--- js/script.js
+++ js/script.js
@@ -1386,6 +1386,57 @@ https://www.instagram.com/reels/Db6sB0oTwtO/
     `),
         closing: "Du bist ein Traum",
         signature: "Dieser Flo 😝"
+    },
+    {
+        id: "2026-09-10",
+        date: "10. September 2026",
+        time: "23:56",
+        title: "Trainingsspieltag",
+        salutation: "Hallo du süße!",
+        paragraphs: toParagraphs(`
+       
+        Einen wunderschönen Bastelabend 
+        wünsch ich dir!
+        Der Alarm hat dich heute wohl gut 
+        ausm Bett gerissen hm?
+        Der Energielevel ist konstanz unter
+        Belastung was net cool ist :c
+        Ich wünsche mir für dich, dass du
+        zum Loyg Tag noch mehr als genug Kraft
+        hast, den Tag vollstens zu genießen 🙏🏼
+
+        Ich freu mich aufjedenfall schon sehr da es
+        mal wieder eine neue Experience wird :D
+        Und yo was hätte ich mir besser vorstellen
+        können als diese Experience mit dir zu teilen 
+        genießen zu können. 🫶🏼
+
+        Außerdem kann ich es kaum erwarten jetzt Führerschein
+        usw. zu machen um einfach mehr für dich da sein zu können.
+        Du ahnst wahrscheinlich gar nicht wie oft ich darüber
+        nachdenke wie ich in deine Richtung ziehen kann.
+        Da bei meinem Job es momentan eh komisch ist und das
+        Gehaltsgespräch noch aussteht, steht ein Jobwechsel 
+        eventuell eh an. 
+
+        Naja wat die Zukunft wohl bringt wissen nicht einmal
+        die Sterne oder so xD
+
+        Was ich mir von den Sternen wünsche ist,
+        dass du soviel Kraft und Liebe bekommst wie
+        du verdienst. Ja du verdienst viel viel mehr
+        als du aktuell vielleicht bekommst!
+        Es werden bessere Zeiten kommen da bin und bleibe
+        ich mir ziemlich sicher! 
+
+        https://www.instagram.com/reels/DcsxJpRhQXt/
+
+        https://www.instagram.com/p/DcOR2wutxKP/?stkn=MWxmNTVtb3Y1OXpoeA%3D%3D
+
+
+    `),
+        closing: "Bis balde",
+        signature: "Dein Flo 🖤💜 "
     }
 ];
 
