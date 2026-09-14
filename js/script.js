63769ee1a3a3342228c722ed46caa50540df42da
Florian Tamm <floriantamm93@users.noreply.github.com>
2026-09-14 23:16:44 +0200

    14 September

--- js/script.js
+++ js/script.js
@@ -1549,6 +1549,62 @@ https://www.instagram.com/reels/Db6sB0oTwtO/
     `),
         closing: "I miss you!",
         signature: "Dein kleiner Flo 🖤 "
+    },
+    {
+        id: "2026-09-14",
+        date: "14. September 2026",
+        time: "22:56",
+        title: "Schnelle Nummer",
+        salutation: "Gud'n Abend!",
+        paragraphs: toParagraphs(`
+       
+        Ich bin total platt in dem Moment wo
+        ich das hier schreibe. Das gleiche
+        gilt aber auch wahrscheinlich für dich 😅
+        Du hast heute deine Küche gebändigt und
+        ich meine Jungs beim Sport.
+
+        Der Tag heute war unerwartet komisch.
+        Erst dachte ich, dass es für dich ein guter
+        Tag wird. Leider war das irgendwie nicht der Fall
+        und es fühlt sich so an als wäre ich
+        Schuld daran. Manchmal kommt es mir so vor,
+        dass meine Verfügbarkeit eher kontraproduktiv ist.
+        Zusätzlich sind die Aussagen bzw. Reels die ich
+        dir zukommen lasse wirklich ernst gemeint.
+        Das dies bei dir nicht so ankommt und es den Eindruck
+        erweckt, dass ich das oberflächlich meine oder so.
+
+        Ein Satz hat mich wirklich schwer getroffen 
+        und er lautete so:
+        Also ich kann dich gebrauchen aber du kannst gerade nicht
+        auf die Art und Weise da sein wie ich dich gebrauchen könnte
+        Damit ist wahrscheinlich gemeint, dass ich in Persona
+        für dich erreichbar bin oder?
+        WEnn das der Fall sit kannst du mir eins glauben.
+        Ich würde die Unterwelt auf den Kopf stellen um das
+        möglich zu machen. Aber ich bin irgendwie in einen Zwiespalt
+        gefangen D:
+        Wenn ich das machen würde denke ich, dass ich dich unter 
+        noch mehr Druck setze 😟
+
+        Das alles ändert nichts daran, dass du für mich
+        wirklich besonders bleibst 🫶🏼🤗
+        ps. ich verstehe, dass diese WOrte für dich teilweise Gift sind
+        und es dir sehr schwer fällt nach all den Sachen,
+        die passiert sind, glauben zu schenken.
+
+        Nunja bin aufjedenfall stolz auf dich, dass du 
+        die Küche und Wäsche heute zu großen Teilen
+        erledigt hast. Wünsche mir vom Herzen,
+        dass es dir jetzt und auch in nächster Zeit wieder guti
+        geht ♥️
+        
+        
+
+    `),
+        closing: "Bis bald!",
+        signature: "Flo"
     }
 ];
 
