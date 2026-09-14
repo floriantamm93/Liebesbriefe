c39daf18f90cd1012adaf7a8be73f22449fe4c62
Florian Tamm <floriantamm93@users.noreply.github.com>
2026-09-14 23:33:29 +0200

    14 September v2

--- js/script.js
+++ js/script.js
@@ -1570,27 +1570,30 @@ https://www.instagram.com/reels/Db6sB0oTwtO/
         und es fühlt sich so an als wäre ich
         Schuld daran. Manchmal kommt es mir so vor,
         dass meine Verfügbarkeit eher kontraproduktiv ist.
-        Zusätzlich sind die Aussagen bzw. Reels die ich
+        und eher ein Fluch ist statt dir zu helfen.
+        Nonetheless sind die Aussagen bzw. Reels die ich
         dir zukommen lasse wirklich ernst gemeint.
         Das dies bei dir nicht so ankommt und es den Eindruck
-        erweckt, dass ich das oberflächlich meine oder so.
+        erweckt, dass ich das oberflächlich meine ist natürlich
+        auch nicht gerade optimal.
 
         Ein Satz hat mich wirklich schwer getroffen 
         und er lautete so:
         Also ich kann dich gebrauchen aber du kannst gerade nicht
         auf die Art und Weise da sein wie ich dich gebrauchen könnte
         Damit ist wahrscheinlich gemeint, dass ich in Persona
-        für dich erreichbar bin oder?
-        WEnn das der Fall sit kannst du mir eins glauben.
+        für dich nicht erreichbar bin oder?
+        Wenn das der Fall ist kannst du mir eins glauben.
         Ich würde die Unterwelt auf den Kopf stellen um das
-        möglich zu machen. Aber ich bin irgendwie in einen Zwiespalt
+        umgekehrt zu machen. Aber ich bin irgendwie in einen Zwiespalt
         gefangen D:
         Wenn ich das machen würde denke ich, dass ich dich unter 
         noch mehr Druck setze 😟
+        und das ist das Letzte was ich möchte.
 
         Das alles ändert nichts daran, dass du für mich
         wirklich besonders bleibst 🫶🏼🤗
-        ps. ich verstehe, dass diese WOrte für dich teilweise Gift sind
+        ps. ich verstehe, dass diese Worte für dich teilweise Gift sind
         und es dir sehr schwer fällt nach all den Sachen,
         die passiert sind, glauben zu schenken.
 
