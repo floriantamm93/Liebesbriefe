93d48350c11538548fd46c3144212f338d23de9c
Florian Tamm <floriantamm93@users.noreply.github.com>
2026-09-17 22:41:49 +0200

    Last patch 17.September

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { letters } from "./letters/index.js?v=2026-09-16-2";
+import { letters } from "./letters/index.js?v=2026-09-17-3";
 
 const openButton = document.querySelector("#open-letter");
 const printButton = document.querySelector("#print-letter");
