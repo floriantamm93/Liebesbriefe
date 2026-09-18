c9be3afb09601c91585a921a61cfd89319416569
Florian Tamm <floriantamm93@users.noreply.github.com>
2026-09-18 23:46:23 +0200

    18 patch 2

--- js/letters/index.js
+++ js/letters/index.js
@@ -33,7 +33,7 @@ import letter32 from "./2026-09-14.js?v=2026-09-16";
 import letter33 from "./2026-09-15.js?v=2026-09-16";
 import letter34 from "./2026-09-16.js?v=2026-09-16";
 import letter35 from "./2026-09-17.js?v=2026-09-17";
-import letter36 from "./2026-09-17.js?v=2026-09-18";
+import letter36 from "./2026-09-18.js?v=2026-09-18";
 
 // Die Reihenfolge hier bestimmt die Archivansicht und den vorausgewählten Brief.
 export const letters = [
